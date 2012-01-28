b9c6237592340497543554e1d21cd34f611e864f
Gillis Van Ginderachter <gvginder@vub.ac.be>
2012-01-28 01:35:10 +0100

    Better command class.

--- scripts/plugins/command.js
+++ scripts/plugins/command.js
@@ -1,23 +1,34 @@
 CommandPlugin = Class.extend();
 
-CommandPlugin.parse = function(message) {
-	if (message.message == "lol") {
-		hooked = new Packet();
-		hooked.writeU8(MessageSpeak.type);
-		double = MessageSpeak.create();
-		double.type = message.type;
-		double.channel = message.channel;
-		double.receiver = message.receiver;
-		double.message = "lolol";
-		double.serialize(hooked);
-		Hook.write(hooked);
-		return true;
-	}
-	else if (message.message == "reload") {
-		Environment.reload();
-		return true;
-	}
-	return false;
+CommandPlugin.Commands = {};
+
+CommandPlugin.Commands["lol"] = function(speak, arguments) {
+	hooked = new Packet();
+	hooked.writeU8(MessageSpeak.type);
+	double = MessageSpeak.create();
+	double.type = speak.type;
+	double.channel = speak.channel;
+	double.receiver = speak.receiver;
+	double.message = "lolol";
+	double.serialize(hooked);
+	Hook.write(hooked);
+};
+
+CommandPlugin.Commands["reload"] = function(speak, arguments) {
+	Environment.reload();
+};
+
+CommandPlugin.parse = function(speak) {
+	if (speak.message.charAt() != "%")
+		return false;
+
+	var arguments = speak.message.substr(1).split(" ");
+	var command = arguments.shift();
+	var callback = CommandPlugin.Commands[command];
+	if (callback != undefined)
+		callback(speak, arguments);
+
+	return true;
 };
 
 OutgoingHandler.registerMessageCallback(MessageSpeak, CommandPlugin.parse);
