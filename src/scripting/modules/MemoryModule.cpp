e60335c07b78e3d27cf4620589c0ff9518225a1c
Gillis Van Ginderachter <gvginder@vub.ac.be>
2012-01-31 19:25:28 +0100

    Removed MemoryObject. It's not necessary

--- src/scripting/modules/MemoryModule.cpp
+++ src/scripting/modules/MemoryModule.cpp
@@ -14,98 +14,22 @@ QString MemoryModule::name() const {
 
 bool MemoryModule::install(ModuleManager* manager) {
 	QScriptEngine* engine = manager->engine();
-	QScriptValue constructor = engine->newFunction(MemoryModule::constructor);
-	constructor.setProperty("readU8", engine->newFunction(MemoryModule::readU8));
-	constructor.setProperty("readU16", engine->newFunction(MemoryModule::readU16));
-	constructor.setProperty("readU32", engine->newFunction(MemoryModule::readU32));
-	constructor.setProperty("readString", engine->newFunction(MemoryModule::readString));
-	engine->globalObject().setProperty(VARIABLE_NAME, constructor, QScriptValue::ReadOnly | QScriptValue::Undeletable);
+	engine->globalObject().setProperty(VARIABLE_NAME, engine->newQObject(this), QScriptValue::ReadOnly | QScriptValue::Undeletable);
 	return true;
 }
 
-QScriptValue MemoryModule::constructor(QScriptContext* context, QScriptEngine* engine) {
-	if (context->isCalledAsConstructor() && context->argumentCount() == 1) {
-		QScriptValue object = context->thisObject();
-		QScriptValue address = context->argument(0);
-		if (address.isNumber()) {
-			return engine->newQObject(object, new MemoryObject(address.toUInt32()), QScriptEngine::ScriptOwnership);
-		}
-	}
-	return context->throwError("invalid call to Memory(Number) constructor");
+quint8 MemoryModule::readU8(quint32 address) {
+	return Memory::readU8(address);
 }
 
-QScriptValue MemoryModule::readU8(QScriptContext* context, QScriptEngine* engine) {
-	if (context->argumentCount() == 1) {
-		QScriptValue address = context->argument(0);
-		if (address.isNumber()) {
-			return Memory::readU8(address.toUInt32());
-		}
-	}
-	return context->throwError("invalid call to readU8(Number)");
+quint16 MemoryModule::readU16(quint32 address) {
+	return Memory::readU16(address);
 }
 
-QScriptValue MemoryModule::readU16(QScriptContext* context, QScriptEngine* engine) {
-	if (context->argumentCount() == 1) {
-		QScriptValue address = context->argument(0);
-		if (address.isNumber()) {
-			return Memory::readU16(address.toUInt32());
-		}
-	}
-	return context->throwError("invalid call to readU16(Number)");
+quint32 MemoryModule::readU32(quint32 address) {
+	return Memory::readU32(address);
 }
 
-QScriptValue MemoryModule::readU32(QScriptContext* context, QScriptEngine* engine) {
-	if (context->argumentCount() == 1) {
-		QScriptValue address = context->argument(0);
-		if (address.isNumber()) {
-			return Memory::readU32(address.toUInt32());
-		}
-	}
-	return context->throwError("invalid call to readU32(Number)");
-}
-
-QScriptValue MemoryModule::readString(QScriptContext* context, QScriptEngine* engine) {
-	if (context->argumentCount() == 1) {
-		QScriptValue address = context->argument(0);
-		if (address.isNumber()) {
-			return Memory::readString(address.toUInt32());
-		}
-	}
-	return context->throwError("invalid call to readString(Number)");
-}
-
-quint16 MemoryObject::position() const {
-	return position_;
-}
-
-void MemoryObject::setPosition(quint16 position) {
-	position_ = position;
-}
-
-void MemoryObject::skip(quint16 count) {
-	position_ += count;
-}
-
-quint8 MemoryObject::readU8() {
-	quint8 value = Memory::readU8(address_ + position_);
-	position_ += 1;
-	return value;
-}
-
-quint16 MemoryObject::readU16() {
-	quint16 value = Memory::readU16(address_ + position_);
-	position_ += 2;
-	return value;
-}
-
-quint32 MemoryObject::readU32() {
-	quint32 value = Memory::readU32(address_ + position_);
-	position_ += 4;
-	return value;
-}
-
-QString MemoryObject::readString(quint16 maximumLength) {
-	QString value = Memory::readString(address_ + position_);
-	position_ += maximumLength;
-	return value;
+QString MemoryModule::readString(quint32 address) {
+	return Memory::readString(address);
 }
--- src/scripting/modules/MemoryModule.h
+++ src/scripting/modules/MemoryModule.h
@@ -21,35 +21,11 @@ public:
 
 	bool install(ModuleManager*);
 
-private:
-	static QScriptValue constructor(QScriptContext*, QScriptEngine*);
-	static QScriptValue readU8(QScriptContext*, QScriptEngine*);
-	static QScriptValue readU16(QScriptContext*, QScriptEngine*);
-	static QScriptValue readU32(QScriptContext*, QScriptEngine*);
-	static QScriptValue readString(QScriptContext*, QScriptEngine*);
-};
-
-class MemoryObject: public QObject {
-	Q_OBJECT
-	Q_PROPERTY(quint16 position WRITE setPosition READ position)
-
-public:
-	MemoryObject(quint32 address, QObject* = 0);
-
-	quint16 position() const;
-	void setPosition(quint16);
-
 public slots:
-	void skip(quint16 = 1);
-
-	quint8 readU8();
-	quint16 readU16();
-	quint32 readU32();
-	QString readString(quint16);
-
-private:
-	quint32 address_;
-	quint16 position_;
+	quint8 readU8(quint32);
+	quint16 readU16(quint32);
+	quint32 readU32(quint32);
+	QString readString(quint32);
 };
 
 #endif /* MEMORYMODULE_H */
