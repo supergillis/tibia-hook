f38115a269b610696e71c26aada6b9aa3a8065aa
Gillis Van Ginderachter <supergillis@gmail.com>
2012-03-04 12:02:25 +0100

    Added license.

--- include/ScriptEngineInterface.h
+++ include/ScriptEngineInterface.h
@@ -1,8 +1,24 @@
+/* Copyright (c) 2012 Gillis Van Ginderachter <supergillis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef SCRIPTENGINEINTERFACE_H_
 #define SCRIPTENGINEINTERFACE_H_
 
 #include <QtGlobal>
 #include <QByteArray>
+#include <QScriptEngine>
 #include <QString>
 
 #include <SenderInterface.h>
@@ -14,7 +30,7 @@ public:
 	ScriptEngineInterface(QObject* parent = 0): QScriptEngine(parent) {}
 	virtual ~ScriptEngineInterface() {}
 
-	virtual SenderInterface* sender() = 0;
+	virtual SenderInterface* sender() const = 0;
 
 	virtual bool reload() = 0;
 	virtual bool require(const QString&) = 0;
