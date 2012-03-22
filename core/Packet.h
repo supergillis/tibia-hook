4bb26769f3e4b3e6aa3c2b2e1a360f0525e3ae7b
Gillis Van Ginderachter <supergillis@gmail.com>
2012-03-22 18:50:44 +0100

    Updated the Packet class. Put logic in the header file and made it a bit more abstract using templates.

--- core/Packet.h
+++ core/Packet.h
@@ -24,27 +24,69 @@
 
 class Packet {
 public:
-	Packet();
+	Packet(): position_(0) {}
 	virtual ~Packet() {}
 
 	virtual quint16 length() const = 0;
 	virtual const quint8* data() const = 0;
 
-	quint16 position() const;
-	void setPosition(quint16);
+	inline quint16 position() const {
+		return position_;
+	}
 
-	void skip(quint16);
-	bool has(quint16) const;
+	inline void setPosition(quint16 position) {
+		position_ = position;
+	}
 
-	quint8 readU8();
-	quint16 readU16();
-	quint32 readU32();
-	quint64 readU64();
-	QString readString();
+	void skip(quint16 count) {
+		position_ += count;
+	}
+
+	inline bool has(quint16 count) const {
+		return length() - position_ >= count;
+	}
+
+	inline quint8 readU8() {
+		return read<quint8, 1>();
+	}
+
+	inline quint16 readU16() {
+		return read<quint16, 2>();
+	}
+
+	inline quint32 readU32() {
+		return read<quint32, 4>();
+	}
+
+	inline quint64 readU64() {
+		return read<quint64, 8>();
+	}
+
+	QString readString() {
+		quint16 length = readU16();
+		if (has(length)) {
+			QString value = QString::fromAscii((const char*) (data() + position_), length);
+			position_ += length;
+			return value;
+		}
+		qWarning() << PACKET_END_OF_FILE;
+		return QString();
+	}
 
 protected:
 	quint16 position_;
 
+private:
+	template<typename T, int size>
+	inline T read() {
+		if (!has(size)) {
+			qWarning() << PACKET_END_OF_FILE;
+			return 0;
+		}
+		T value = *((T*) (data() + position_));
+		position_ += size;
+		return value;
+	}
 };
 
 #endif /* PACKET_H_ */
